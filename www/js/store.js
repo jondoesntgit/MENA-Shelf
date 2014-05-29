a2788b19555fe6c50c94ee687d226b14b35261f0
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2014-05-29 14:07:03 +0300

    Rewrote code for accessing the online store.
    Currently testing on local machine before testing on remote server.

---
+++ www/js/store.js
@@ -0,0 +1,115 @@
+/*$(document).ready(function(){
+   var txt = "hi";
+   $.("main-container")
+   $.getJSON("http://jamwheeler.com/sharing-solutions/store.php",function(result){
+     store=$.parseJSON(result)
+     $.each(store, function(k, v){
+       txt = "<a href='" + v.url + "'><button>" + v.name + "</button></a><br/>"
+     });
+   });
+   $("#main-container").append(txt);
+   $(document).append("Hello");
+ }); 
+
+*/
+
+// Library = all languages
+// Cataloge = stuff for one specific language
+// Book = a specific instance
+
+
+var lang = "en";
+var library;
+var url="http://192.168.16.219/~wheelerj/adventistmena/menashelf/";
+
+function getDefaultLang() {
+  return lang;
+}
+
+/**
+ * Takes a JSON argument and displays download links for the selected language
+ */
+
+function loadContent(language) {
+
+  // Grab the catalogue object based on language
+  var catalog;
+  switch (language)
+  {
+    // Arabic
+    case "ar":
+    catalog = library.ar
+    break;
+ 
+    // English (Default)
+    default:
+    case "en":
+    catalog = library.en
+    break;
+ 
+    // Farsi
+    case "fa":
+    catalog = library.fa
+    break;
+ 
+    // French
+    case "fr":
+    catalog = library.fr
+    break;
+ 
+    // Turkish
+    case "tr":
+    catalog = library.tu
+  }
+ 
+  // Generate code to display in main container
+  var storeHTML = "";
+  
+  if (typeof(catalog) == "undefined") // If there is no such language in the JSON object
+  {
+    storeHTML = "That language is not supported yet."
+  }
+  else if (catalog.length >=1) //If there is at least one book
+  {
+    for(var i = 0; i < catalog.length; i++) 
+    {
+        var book = catalog[i];
+        storeHTML += "<a href='" + url + book.url + "'>" +  book.title + "</a>";
+    }
+  } 
+  else if (catalog.length == 0) { //If there is no books in this language
+    storeHTML = "Sorry, there are no books available in this language."
+  }
+  else {
+    storeHTML = "An error occured. Please contact the developer." 
+  }
+  
+  
+  
+ 
+  $( "#main-container" ).html(storeHTML);
+};
+
+
+$( document ).ready(function()
+{
+   //$.getJSON("http://jamwheeler.com/sharing-solutions/store.php",function(result){
+   console.log(url + "store/remote.php");
+   $.getJSON(url + "store/remote.php",function(result)
+   {
+     library = result;
+   });
+   console.log(url + "store/remote.php");
+
+});
+
+function loadStore() {
+  $( "#store-results-list" ).append("It works");
+//  $().getElementById("main").innerHTML="Hello"
+}
+
+function updateLanguage() 
+{
+  lang=$( "#language-selector").val()
+  loadContent(lang)
+}
