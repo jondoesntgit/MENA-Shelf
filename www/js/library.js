d313dd9e017e69149f67bed9f4a5ca562204cf30
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2014-06-16 17:06:14 +0300

    Changed jQuery .html to .append

--- www/js/library.js
+++ www/js/library.js
@@ -1,9 +1,13 @@
 loadLibrary = function(event){
   $( "#library" ).append("Loading library...")
-  //console.log('Attempting download');
-  //downloadData('en/asdf.xml');
   console.log('Attempting to load file')
   var b = new FileManager()
-  b.download_file('http://menashelf.menaulit.org','menashelf','asdf.xml',console.log('finished downloading'));
-  b.read_file('menashelf','asdf.xml',function(result){$("#library").append(result); console.log('loaded file successfully')},console.log('something went wrong'));
+  b.read_file(
+    'menashelf',
+    'asdf.xml',
+    function(result){
+      $("#library").append(result); 
+      console.log('loaded file successfully')},
+      console.log('something went wrong')
+  );
 }
