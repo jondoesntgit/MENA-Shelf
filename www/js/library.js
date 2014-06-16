213d65f393e7fdbcc0dfdd02807fafcbd45106c0
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2014-06-16 14:55:49 +0300

    Testing download capabilities inside of library js

--- www/js/library.js
+++ www/js/library.js
@@ -4,5 +4,6 @@ loadLibrary = function(event){
   //downloadData('en/asdf.xml');
   console.log('Attempting to load file')
   var b = new FileManager()
+  b.download_file('http://menashelf.menaulit.org','menashelf','asdf.xml',console.log('finished downloading'));
   b.read_file('menashelf','asdf.xml',function(result){$("#library").append(result); console.log('loaded file successfully')},console.log('something went wrong'));
 }
